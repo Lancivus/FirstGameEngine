29bfa5ef5c8ec53545364010e02abfe8fcaaf728
Lawrence Fragakis (s4915540) <s4915540@bournemouth.ac.uk>
2018-11-23 11:53:07 +0000

    Initial CES Framework

---
+++ SRC/FirstGameEngine/FirstGameEngine.h
@@ -0,0 +1,8 @@
+#ifndef FIRSTGAMEENGINE_FIRSTGAMEENGINE_H
+#define FIRSTGAMEENGINE_FIRSTGAMEENGINE_H
+
+#include "Core.h"
+#include "Entity.h"
+#include "Component.h"
+
+#endif
--- SRC/Game/Main.cpp
+++ SRC/Game/Main.cpp
@@ -1,8 +1,42 @@
+#include <FirstGameEngine/FirstGameEngine.h>
+
 #include <iostream>
+
+class TestScreen : public FirstGameEngine::Component
+{
+public:
+	void onInit(std::string color)
+	{
+		//std::cout << "onInit " << color << std::endl;
+	}
+
+	void onBegin()
+	{
+		//std::cout << "onBegin" << std::endl;
+	}
+
+	void onTick()
+	{
+		//std::cout << "onTick" << std::endl;
+	}
+
+	void onDisplay()
+	{
+		//std::cout << "onTick" << std::endl;
+	}
+};
 
 int main()
 {
-    std::cout << "Hello World" << std::endl;
+	std::shared_ptr<FirstGameEngine::Core> c = FirstGameEngine::Core::initialize();
 
-    return 0;
+	std::shared_ptr<FirstGameEngine::Entity> e = c->addEntity();
+
+	std::shared_ptr<TestScreen> ts = e->addComponent<TestScreen>("Green");
+	std::shared_ptr<FirstGameEngine::MeshRenderer> mr = e->addComponent<FirstGameEngine::MeshRenderer>();
+	std::shared_ptr<FirstGameEngine::MeshRenderer> mr2 = e->getComponent<FirstGameEngine::MeshRenderer>();
+
+	c->start();
+
+	return 0;
 }
